08205149a34df023af04fa2d48c73d0a7135d69a
unknown <zacharysamiyoussef@gmail.com>
2021-12-10 13:49:23 -0500

    Implement day 2 part 2

--- day2.rkt
+++ day2.rkt
@@ -2,6 +2,8 @@
 
 (require "util.rkt")
 
+; Part 1 -----------------------------------------------------------
+
 ; apply-movements : [List (list String Number)] -> (list Number Number)
 ; Takes a list of instructions and returns the horizontal position & depth afterwards
 (define (apply-movements instructions)
@@ -20,5 +22,23 @@
   (apply (λ [command n] (list command (string->number n)))
          (string-split line " ")))
 
-(module+ main
+#;(module+ main
   (display (apply * (apply-movements (read-lines parse-command)))))
+
+; Part 2 -----------------------------------------------------------
+
+; apply-movements-v2 : [List (list String Number)] -> (list Number Number)
+; Same as before, but modified to keep track of aim
+(define (apply-movements-v2 instructions)
+  (for/fold ([pos 0]
+             [depth 0]
+             [aim 0]
+             #:result (list pos depth))
+            ([instr instructions])
+    (match instr
+      [(list "down" n) (values pos depth (+ aim n))]
+      [(list "up" n) (values pos depth (- aim n))]
+      [(list "forward" n) (values (+ pos n) (+ depth (* aim n)) aim)])))
+
+(module+ main
+  (display (apply * (apply-movements-v2 (read-lines parse-command)))))
