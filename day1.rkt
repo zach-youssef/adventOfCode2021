8ad25d99bedb3f7de7b363f53ebaf6dfc3124762
unknown <zacharysamiyoussef@gmail.com>
2021-12-10 13:31:48 -0500

    Implement day1 part 2

--- day1.rkt
+++ day1.rkt
@@ -1,6 +1,9 @@
 #lang racket
 
 (require "util.rkt")
+(require racket/hash)
+
+; Part 1 -----------------------------------------------------------
 
 ; count-increases : [NE-List Number] -> Nat
 ; Takes a sequence of numbers and returns the number of instances
@@ -13,5 +16,22 @@
     (values (if (> cur-elem last-elem) (add1 count) count)
             cur-elem)))
 
-(module+ main
+#;(module+ main
   (display (count-increases (read-lines string->number))))
+
+; Part 2 -----------------------------------------------------------
+
+; sliding-depths : [NEList Number] -> [NEList Number]
+; Takes a list of measurements and computes the three-mesaurement sliding list of sums
+(define (sliding-depths lon)
+  (if (and (cons? lon)
+           (cons? (cdr lon))
+           (cons? (cddr lon)))
+      (cons (+ (car lon)
+               (cadr lon)
+               (caddr lon))
+            (sliding-depths (rest lon)))
+      '()))
+
+(module+ main
+  (display (count-increases (sliding-depths (read-lines string->number)))))
