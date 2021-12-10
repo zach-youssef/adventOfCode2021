fa1d274dd7236327d01585de88070c596d0c7e09
unknown <zacharysamiyoussef@gmail.com>
2021-12-10 14:59:45 -0500

    Implement day 3 part 2

--- day3.rkt
+++ day3.rkt
@@ -2,6 +2,8 @@
 
 (require "util.rkt")
 
+; Part 1 -----------------------------------------------------------
+
 ; parse-binary : String -> [List Boolean]
 (define (parse-binary line)
   (map (λ [x] (char=? #\1 x)) (string->list line)))
@@ -17,7 +19,7 @@
 (define (most-common-bit lob)
   (for/fold ([ones 0]
              [zeros 0]
-             #:result (> ones zeros))
+             #:result (>= ones zeros))
             ([bit lob])
     (if bit
         (values (add1 ones) zeros)
@@ -41,5 +43,33 @@
   (define epsilon (opposite gamma))
   (* (binary->decimal gamma) (binary->decimal epsilon)))
 
-(module+ main
+#;(module+ main
   (power-consumption (read-lines parse-binary)))
+
+; Part 2 -----------------------------------------------------------
+
+; selector : [List [List Boolean]] Boolean -> [List Boolean]
+; Selects the single binary number from a list based on the following criteria
+; - Find the most (or least if flag is true) common bit in position n
+; - Eliminate all values where the bit does not match that bit in position n
+; - If there remains more than 1 value, recur
+(define (selector values [flag #f])
+  (define (selector/acc values n)
+    (if (empty? (rest values))
+        (first values)
+        (let* ([pos-vals (map (λ [val] (list-ref val n)) values)]
+               [selector-bit (if flag
+                                (not (most-common-bit pos-vals))
+                                (most-common-bit pos-vals))]
+              #;[debug (and (display values) (display pos-vals) (display "\n"))])
+          (selector/acc (filter (λ [val] (not (xor selector-bit (list-ref val n)))) values)
+                        (add1 n)))))
+  (selector/acc values 0))
+
+; life-support-rating : [List [List Boolean]] -> Number
+(define (life-support-rating values)
+  (* (binary->decimal (selector values))
+     (binary->decimal (selector values #t))))
+
+(module+ main
+  (display (life-support-rating (read-lines parse-binary))))
