4a716ef45c839eef57652a62413ae802b8539450
unknown <zacharysamiyoussef@gmail.com>
2021-12-21 22:19:39 -0500

    This is too slow and does not work

--- day19.rkt
+++ day19.rkt
@@ -1,53 +1,69 @@
 #lang racket
 
-; Brute force to "merge" data from scanners n and n+1:
-; For each possible "orientation" of scanner n+1
-;     For each possible pairing of beacons from n and n+1
-;        If 12 or more pairs have the same dx, dy, and dz
-;            "Merge" the datasets by applying that d(x,y,z) to each beacon and scanner in n+1
-;        Else
-;            Keep looping
+(require "util.rkt")
+(require math/matrix)
 
-(struct dataset [beacons scanners])
-; A Position is a (list Int Int Int)
-; Representing an X, Y, Z coordinate
+; Sample math from given input
 
-; A Dataset is a (dataset [Set Position] [Set Position])
-; Representing beacons and scanners on the same coordinate system
+(define S0 (matrix-transpose (matrix [[-618 -824 -621 1]
+                                      [-537 -823 -458 1]
+                                      [-447 -329 318 1]
+                                      [404 -588 -901 1]])))
+(define S1 (matrix-transpose (matrix [[686 422 578 1]
+                                      [605 423 415 1]
+                                      [515 917 -361 1]
+                                      [-336 658 858 1]])))
 
-(define (flip f) (λ [lon] (- (f lon))))
+(define T (matrix* S0 (matrix-inverse S1)))
 
-(define (flip-perms lof)
-  (if (empty? (rest lof)) (list (list (first lof)) (list (λ [lon] (- ((first lof) lon)))))
-      (append (map (λ [llof] (cons (first lof) llof)) (flip-perms (rest lof)))
-              (map (λ [llof] (cons (flip (first lof)) llof)) (flip-perms (rest lof))))))
 
-(define ALL-ORIENTATIONS (foldr (λ [l ll] (append (flip-perms l) ll))
-                                '()
-                                (list (list first second third)
-                                      (list first third second)
-                                      (list second first third)
-                                      (list second third first)
-                                      (list third first second)
-                                      (list third second first))))
+; Find 4 points that are the same distance from each other in both S0 and S1
+; Do the matrix math find T
+; Use T move all the points in S1 to S0
+; Repeat
 
-(define (apply-orient orientation pos)
-  (map (λ [f] (f pos)) orientation))
 
-(define (merge d1 d2)
-  (for/or ([orientation ALL-ORIENTATIONS])
-    (let* ([rotate (λ [pos] (apply-orient orientation pos))]
-          [d-rotate (dataset (list->set (set-map (dataset-beacons d2) rotate))
-                             (list->set (set-map (dataset-scanners d2) rotate)))]
-          [displacement (find-displacement d1 d-rotate)])
-      (and displacement
-           (dataset (set-union (dataset-beacons d1)
-                               (list->set (set-map (dataset-beacons d-rotate) (λ [b] (apply-displacement b displacement)))))
-                    (set-union (dataset-scanners d1)
-                               (list->set (set-map (dataset-scanners d-rotate) (λ [s] (apply-displacement s displacement))))))))))
 
-(define (find-displacement d1 d2)
-  #f)
+; find-relative-distances : [List (list x y z)] -> [HashMap  ((list x y z), (list x y z)) -> d]
+(define (find-relative-distances lop)
+  (for*/fold ([distances (hash)])
+             ([from lop]
+              [to (member from lop)])
+    (if (equal? from to)
+        distances
+        (hash-set distances (set from to) (distance from to)))))
 
-(define (apply-displacement pos displacement) #f)
+(define (distance from to)
+  (sqrt (foldr + 0 (map sqr (map - from to))))) ; remove sqrt if equality issues arise
 
+; matching-points : [List (list x y z)] [List (list x y z)]  -> [List (list (list x y z) (list x y z))]?
+(define (matching-points l1 l2)
+  #;(define l1-dist (find-relative-distances l1))
+  #;(define l2-dist (find-relative-distances l2))
+  ; acc :  [HashMap (list x y z) -> (list x y z)] [Set (list (list x y z) (list x y z))] ->  [HashMap (list x y z) -> (list x y z)]?
+  (define (acc mapping unused)
+    (if (= 0 (set-count unused)) mapping
+        (for/or ([pair unused])
+          (let ([new-mapping (hash-set mapping (first pair) (second pair))])
+            (if (valid? new-mapping)
+                (acc new-mapping (set-remove unused pair))
+                #f)))))
+  (acc (hash) (list->set (all-pairs l1 l2))))
+
+; valid?  : [HashMap (list x y z) -> (list x y z)] [HashMap  ((list x y z), (list x y z)) -> d] ^2 -> Boolean
+(define (valid? mapping)
+  (and (let* ([values (hash-values mapping)]
+              [value-set (list->set values)])
+         (= (length values) (set-count value-set))) ; Mapping is bijective
+       (= (apply + (hash-values (find-relative-distances (hash-keys mapping))))
+          (apply + (hash-values (find-relative-distances (hash-values mapping)))))))
+
+; all-pairs [List X] [List Y] -> [List (list X Y)]
+(define (all-pairs lox loy)
+  (for*/list ([x lox]
+              [y loy])
+    (list x y)))
+
+; parse-lop : -> [List (list X Y Z)]
+(define (parse-lop)
+  (read-lines (λ [line] (map string->number (string-split line ",")))))
